72be5dea58c4dca4937fd05cc4f9e278bb4f0916
Kartik Sabharwal <kartiksabharwal7@gmail.com>
2024-03-21 23:05:29 -0500

    updated licence information on the two new files

--- pysmt/solvers/cvc5.py
+++ pysmt/solvers/cvc5.py
@@ -1,8 +1,3 @@
-#
-# This file is part of pySMT.
-#
-#   Copyright 2014 Andrea Micheli and Marco Gario
-#
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
 #   You may obtain a copy of the License at
@@ -14,7 +9,7 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-#
+
 from __future__ import absolute_import
 
 from pysmt.exceptions import SolverAPINotFound
